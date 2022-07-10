1cf4b7a3a7d1f13dcc37ae5af0ca78e309a5097a
aminmallol <aminm-34@hotmail.fr>
2022-07-10 19:56:39 +0100

    Added error handling using try/catch block with fetch and async/await

--- src/App.js
+++ src/App.js
@@ -6,31 +6,48 @@ import './App.css';
 const handleDataDispatch = (state, action) => {
   if (action.type === "DATA") {
     console.log(action.movieData);
-    return { movieData: action.movieData, hasData: action.movieData.length > 0, isLoading: false }
+    return { movieData: action.movieData, hasData: action.movieData.length > 0, isLoading: false, error: null }
   }
 
   if (action.type === "LOADING") {
     return { ...state, isLoading: true }
   }
+
+  if (action.type === "RESET-ERROR") {
+    return { ...state, error: null }
+  }
+
+  if (action.type === "SET-ERROR") {
+    return { ...state, error: action.error, isLoading: false, }
+  }
 }
 
 function App() {
-  const [data, dataDispatch] = useReducer(handleDataDispatch, { movieData: [], hasData: false, isLoading: false })
+  const [data, dataDispatch] = useReducer(handleDataDispatch, { movieData: [], hasData: false, isLoading: false, error: null })
 
   const fetchMoviesHandler = async (movie) => {
+    dataDispatch({ type: "RESET-ERROR" })
     dataDispatch({ type: "LOADING" })
-    const res = await fetch(`https://swapi.dev/api/films/${movie}`)
-    const data = await res.json()
 
-    const transformedMovies = data.results.map(movie => {
-      return {
-        id: movie.episode_id,
-        title: movie.title,
-        openingText: movie.opening_crawl,
-        releaseDate: movie.release_date
-      }
-    })
-    dataDispatch({ type: "DATA", movieData: transformedMovies })
+    try { // ! TRY TO EXECUTE A RISKY PIECE OF CODE
+      const res = await fetch(`https://swapi.dev/api/films/${movie}`)
+      console.log(res);
+      if (res.status === 404) { throw new Error("This endpoint doesn't exist. Double check your fetch URL and try again.") } // ! HANDLING ERROR
+      const data = await res.json()
+
+      const transformedMovies = data.results.map(movie => {
+        return {
+          id: movie.episode_id,
+          title: movie.title,
+          openingText: movie.opening_crawl,
+          releaseDate: movie.release_date
+        }
+      })
+      dataDispatch({ type: "DATA", movieData: transformedMovies })
+    } catch (error) { // ! CATCH ERROR IF THERE IS ONE
+      dataDispatch({ type: "SET-ERROR", error: error })
+      console.log(error);
+    }
   }
 
   console.log(data);
@@ -40,9 +57,10 @@ function App() {
         <button onClick={() => fetchMoviesHandler("")}>Fetch Movies</button>
       </section>
       <section>
-        {(data.hasData) && <MoviesList movies={data.movieData} />}
-        {(data.isLoading) && <p>Loading.. Please wait</p>}
-        { !data.hasData && !data.isLoading && <p>Found no movies</p>}
+        {data.hasData && !data.error && <MoviesList movies={data.movieData} />}
+        {data.isLoading && <p>Loading.. Please wait</p>}
+        {!data.hasData && !data.isLoading && !data.error && <p>Found no movies</p>}
+        {!data.hasData && !data.isLoading && data.error && <p><strong>AN ERROR OCCURED:</strong> <br /> {data.error.message}</p>}
       </section>
     </React.Fragment>
   );
